98ff96695acd56974f5f907157c3161778ab2869
ajischaniagoid <43314095+ajischaniagoid@users.noreply.github.com>
2018-09-23 01:39:47 +0800

    Delete googledp.js

--- googledp.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function()
-{
-var aa=$("#footer-dp").val();
-if (aa == null) {
-    window.location.href = "https://dagangpulsa.com/";
-};
- $("#footer-dp").attr("href","https://dagangpulsa.com/");
-});
