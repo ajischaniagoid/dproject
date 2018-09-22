ac8ee1ac742c80cb137f83ae7eee8f0e419a7132
ajischaniagoid <43314095+ajischaniagoid@users.noreply.github.com>
2018-09-23 01:39:25 +0800

    Add files via upload

---
+++ googledp.js
@@ -0,0 +1,8 @@
+$(document).ready(function()
+{
+var aa=$("#footer-dp").val();
+if (aa == null) {
+    window.location.href = "https://dagangpulsa.com/";
+};
+ $("#footer-dp").attr("href","https://dagangpulsa.com/");
+});
